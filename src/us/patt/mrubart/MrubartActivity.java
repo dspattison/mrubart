deea2255c0bef4e625b8b7eff9000b8829687115
David Pattison <dspattison@gmail.com>
2012-01-15 12:15:40 -0800

    list view works

--- src/us/patt/mrubart/MrubartActivity.java
+++ src/us/patt/mrubart/MrubartActivity.java
@@ -1,13 +1,35 @@
 package us.patt.mrubart;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.TextView;
 
-public class MrubartActivity extends Activity {
+public class MrubartActivity extends ListActivity {
     /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+
+	  setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, STATIIONS));
+
+	  ListView lv = getListView();
+	  lv.setTextFilterEnabled(true);
+
+	  lv.setOnItemClickListener(new OnItemClickListener() {
+	    public void onItemClick(AdapterView<?> parent, View view,
+	        int position, long id) {
+	      // When clicked, show a toast with the TextView text
+	      Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+	          Toast.LENGTH_SHORT).show();
+	    }
+	  });
+	}
+	static final String[] STATIIONS = new String[]  {"NBRK", "EMBK"};
 }
